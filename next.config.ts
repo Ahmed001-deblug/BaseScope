85a0007595dea20a7021b92a3c635faa3166e901
Ahmed001-deblug <adegbeahmed001@gmail.com>
2026-03-23 13:34:21 -0700

    fix vercel build issue

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
